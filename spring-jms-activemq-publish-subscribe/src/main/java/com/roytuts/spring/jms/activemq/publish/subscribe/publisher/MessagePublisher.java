e0c21ecad6cb8b57889fedf96b3b444a7b0d2276
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-11-28 16:59:05 +0530

    Add files via upload

---
+++ spring-jms-activemq-publish-subscribe/src/main/java/com/roytuts/spring/jms/activemq/publish/subscribe/publisher/MessagePublisher.java
@@ -0,0 +1,17 @@
+package com.roytuts.spring.jms.activemq.publish.subscribe.publisher;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessagePublisher {
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
+	public void sendMessage(final String message) {
+		jmsTemplate.convertAndSend(message);
+	}
+
+}
